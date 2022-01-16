Fix logging of calculation energy + add reading of magnetization output from OOMMF

programms/MR/calcEnergyOfPlatform/class_generator_energyDistribution.py
@@ -19,13 +19,14 @@ class GeneratorEnergyDistribution:
     def __init__(self, mTop=None, mBottom=None, kTop=None, kBottom=None, jEx=None, jAf=None, \
                 xCell=None, yCell=None, zCell=None, \
                 xCount=None, yCount=None, zCount=None, \
-                fieldDistrFilePath=None, \
+                fieldDistrFilePath=None, magnDistrFilePath = None, \
                 outPath=None):
 
-        # Dict with input field distribution (top layer)
+        # Dict with input field distribution (top and bottom layers)
         self.fieldOfNPDataTop = defaultdict(list) # [m, m, m] -> [A/m, A/m, A/m]
-        # Dict with input field distribution (bottom layer)
         self.fieldOfNPDataBottom = defaultdict(list) # [m, m, m] -> [A/m, A/m, A/m]
+        self.magnDataTop = defaultdict(list) # [m, m, m] -> [A/m, A/m, A/m]
+        self.magnDataBottom = defaultdict(list) # [m, m, m] -> [A/m, A/m, A/m]
 
         # Dict with output energy distribution
         self.energyDistrData = defaultdict(float) # [m, m] -> [J/m^2]
@@ -50,28 +51,37 @@ class GeneratorEnergyDistribution:
         # input/output options
         assert(fieldDistrFilePath)
         assert(outPath)
+        assert(magnDistrFilePath)
         self.outPath = outPath
         self.fieldDistrFilePath = fieldDistrFilePath
+        self.magnDistrFilePath = magnDistrFilePath
 
         # convert units
         #self.convertUnits()
 
-    def correctReadingInputLine(self, line):
+    def correctReadingInputLine(self, line, mode):
         '''
         Input Format: ' ' + '  '.join(list(map(str, coords))) + '  ' + '  '.join(list(map(str, values)))
+        mode: {'field', 'magn'} - type of input file
         '''
-        line = line.strip().split('  ')
-        coords = list(map(float, line[:3]))
-        fieldComponents = list(map(float, line[3:]))
+        line = list(filter(lambda el: len(el) > 0, line.strip().split(' ')))
+        coords = list(map(lambda el: np.round(float(el), 14), line[:3])) # FIXME ацкий хак
+        vectorComponents = list(map(float, line[3:]))
         # 1 - top split
         if abs((coords[2] - self.SPLIT_TOP)) < self.EPS:
-            self.fieldOfNPDataTop[tuple(coords)] = fieldComponents
+            logging.debug(f"[READING of {mode}]: top point")
+            if (mode == 'field'):
+                self.fieldOfNPDataTop[tuple(coords)] = vectorComponents
+            elif (mode == 'magn'):
+                self.magnDataTop[tuple(coords)] = vectorComponents
         else:
-            pass
-            #logging.info(f"[READING]: difference between z-components: from file = {coords[2]}, diff = {coords[2] - self.SPLIT_TOP}")
+            logging.debug(f"[READING of {mode}]: difference between z-components: from file = {coords[2]} our = {self.SPLIT_TOP}, diff = {coords[2] - self.SPLIT_TOP}")
         # 2 - bottom split
         if abs((coords[2] - self.SPLIT_BOTTOM)) < self.EPS:
-            self.fieldOfNPDataBottom[tuple(coords)] = fieldComponents        
+            if (mode == 'field'):
+                self.fieldOfNPDataBottom[tuple(coords)] = vectorComponents
+            elif (mode == 'magn'):
+                self.magnDataBottom[tuple(coords)] = vectorComponents
 
     def readingFieldOfNps(self):
         with codecs.open(self.fieldDistrFilePath, 'r', encoding='utf-8') as fin:
@@ -82,15 +92,30 @@ class GeneratorEnergyDistribution:
                 if (counter <= amountOfAuxiliaryInfoRows):
                     continue
                 try:
-                    self.correctReadingInputLine(line)
+                    self.correctReadingInputLine(line, mode = 'field')
                 except:
                     pass
-        logging.info(f"[READING]:count of readed rows: in top layer : {len(list(self.fieldOfNPDataTop.keys()))}, in bottom layer : {len(list(self.fieldOfNPDataBottom.keys()))}")
+        logging.info(f"[READING field]: count of readed rows: in top layer : {len(list(self.fieldOfNPDataTop.keys()))}, in bottom layer : {len(list(self.fieldOfNPDataBottom.keys()))}")
+
+    def readingMagnetization(self):
+        with codecs.open(self.magnDistrFilePath, 'r', encoding='utf-8') as fin:
+            amountOfAuxiliaryInfoRows = 29
+            counter = 0
+            for line in fin:
+                counter += 1
+                if (counter <= amountOfAuxiliaryInfoRows):
+                    continue
+                try:
+                    self.correctReadingInputLine(line, mode = 'magn')
+                except:
+                    pass
+        logging.info(f"[READING magnetization]: count of readed rows: in top layer : {len(list(self.magnDataTop.keys()))}, in bottom layer : {len(list(self.magnDataBottom.keys()))}")
+
 
     def calculateEnergyDistribution(self):
         '''
         Calculate energy distribution:
-        - output file format: X[nm] \t Y[nm] \t E(X,Y)[J/m^2]
+        - output file format: X[m] \t Y[m] \t E(X,Y)[J/m^2]
         '''
         # FIXME: keys of fieldOfNPDataTop and fieldOfNPDataBottom are identical, may be combine them?
         for (coordsTop, coordsBottom) in tqdm(zip(self.fieldOfNPDataTop.copy().keys(), self.fieldOfNPDataBottom.copy().keys())):
@@ -99,25 +124,30 @@ class GeneratorEnergyDistribution:
     def calculateEnergyDensityInPoint(self, coordsTop, coordsBottom):
         '''
         Calculate energy distribution at point (x, y, z)
-        E_total = E_zeeman + E_exchange + E_af + E_anis
-        E_zeeman = -m_0 * (Htop*h_top*Mstop*cos(teta_top) + Hbot*h_bot*Msbot*cos(teta_bot))
+        E_total = E_zeeman + E_af + E_anis
+        E_zeeman = -m_0 * ( (Htop,Mtop)*h_top + (Hbot,Mbot)*h_bot )
         E_anis = -Ks_top * (cos(teta_top))^2 - Ks_bot * (cos(teta_bot))^2
-        E_af = -J_af * cos(teta_top - teta_bot)
-        '''
+        E_af = -J_af * cos(teta_between)
+        '''        
+        # FIXME assertion
+        if (len(self.magnDataTop[coordsTop]) == 0):
+            logging.info(f"[ENERGY]: coords {coordsTop} doesn't locate in magnDataTop")
+        if (len(self.magnDataBottom[coordsBottom]) == 0):
+            logging.info(f"[ENERGY]: coords {coordsBottom} doesn't locate in magnDataBottom")
+
         # Zeeman energy
-        cos_teta_top = np.dot(coordsTop, [0,0,1])/np.sqrt(np.sum([el**2 for el in coordsTop]))
-        cos_teta_bottom = np.dot(coordsBottom, [0,0,1])/np.sqrt(np.sum([el**2 for el in coordsBottom]))
-        H_top = np.linalg.norm(self.fieldOfNPDataTop[coordsTop])
-        H_bottom = np.linalg.norm(self.fieldOfNPDataTop[coordsBottom])
-        zeemanEnergyDensity = -self.MU_0 * (H_top*self.HEIGHT_TOP*self.mTop*cos_teta_top + H_bottom*self.HEIGHT_BOTTOM*self.mBottom*cos_teta_bottom)
+        m_h_top = np.dot(self.magnDataTop[coordsTop], self.fieldOfNPDataTop[coordsTop])
+        m_h_bot = np.dot(self.magnDataBottom[coordsBottom], self.fieldOfNPDataBottom[coordsBottom])    
+        zeemanEnergyDensity = -self.MU_0 * (m_h_top * self.HEIGHT_TOP + m_h_bot * self.HEIGHT_BOTTOM)
         # Anisotropy energy
         K_s_top = self.kTop*self.HEIGHT_TOP # [J/m^3] -> [J/m^2]
         K_s_bottom = self.kBottom*self.HEIGHT_BOTTOM # [J/m^3] -> [J/m^2]
+        cos_teta_top = np.dot(coordsTop, [0,0,1])/np.sqrt(np.sum([el**2 for el in coordsTop]))
+        cos_teta_bottom = np.dot(coordsBottom, [0,0,1])/np.sqrt(np.sum([el**2 for el in coordsBottom]))
         anisEnergyDensity = -K_s_top*cos_teta_top**2 -K_s_bottom*cos_teta_bottom**2
         # Interlayer AF exchange energy
-        teta_top = np.arccos(cos_teta_top)
-        teta_bottom = np.arccos(cos_teta_bottom)
-        interlayerEnergyDensity = -self.jAf * np.cos(teta_top - teta_bottom)
+        teta_between = np.dot(self.magnDataTop[coordsTop], self.magnDataBottom[coordsBottom]) / ( np.linalg.norm(self.magnDataTop[coordsTop]) * np.linalg.norm(self.magnDataBottom[coordsBottom]) )
+        interlayerEnergyDensity = -self.jAf * np.cos(teta_between)
         # summation
         return zeemanEnergyDensity + anisEnergyDensity + interlayerEnergyDensity
 